ed6cd09f92f4e67f8742d78e77642c287772eb8d
Ryan Wilson <ryanwilson.m@gmail.com>
2009-07-26 21:24:58 -0700

    added arrays taglib functions

---
+++ lib/src/java/org/j2free/jsp/el/Arrays.java
@@ -0,0 +1,18 @@
+/*
+ * Arrays.java
+ *
+ * Copyright (c) 2009 FooBrew, Inc.
+ */
+package org.j2free.jsp.el;
+
+/**
+ *
+ * @author Ryan Wilson
+ */
+public class Arrays {
+
+    public static int length(Object[] arr) {
+        return arr == null ? 0 : arr.length;
+    }
+    
+}
