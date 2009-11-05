16735060d27509ff54442775ee0886250410c77b
Ryan Wilson <ryanwilson.m@gmail.com>
2009-11-05 14:14:18 -0800

    functor test

---
+++ lib/src/java/org/j2free/util/Functor.java
@@ -0,0 +1,13 @@
+package org.j2free.util;
+
+/**
+ * This is just a shell which can be instantiated
+ * and then defined inline. e.g.
+ * 
+ * @author Ryan Wilson
+ */
+public interface Functor<T> {
+
+    public T run();
+
+}
